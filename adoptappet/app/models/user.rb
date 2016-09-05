da050f805aaad8eba5b158333e145b5c6db4fd12
Fichberg <rfichberg@gmail.com>
2016-09-05 02:46:49 -0300

    Starting an attempt to add user avatar along with devise

--- adoptappet/app/models/user.rb
+++ adoptappet/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg image/png)
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
