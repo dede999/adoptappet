5091f7670a211042d2574d4ab4fd6dc248d08c01
Fichberg <rfichberg@gmail.com>
2016-10-10 17:55:59 -0300

    Shelter Edit. Fixed a route bug within the all view redirection to show

--- adoptappet/app/controllers/shelters_controller.rb
+++ adoptappet/app/controllers/shelters_controller.rb
@@ -14,7 +14,7 @@ class SheltersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+    @shelter = Shelter.find(params[:id])
   end
 
   def index
@@ -30,5 +30,17 @@ class SheltersController < ApplicationController
   def show
     @user = current_user
     @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    @user = current_user
+    @shelter = Shelter.find(params[:id])
+
+    if @shelter.update_attributes(params.require(:shelter).permit(:name, :address, :phone, :pets_available, :about))
+      flash[:notice] = 'Shelter was successfully updated.'
+      redirect_to user_shelter_path(@user.id, @shelter.id)
+    else
+      render "edit"
+    end
   end
 end
