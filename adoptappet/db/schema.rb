4206f465729ce8e8252052e6c86f8664f4636cdc
dede999 <yankees666suck>
2016-11-05 02:44:24 -0200

    Começando a criar views

--- adoptappet/db/schema.rb
+++ adoptappet/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010154736) do
+ActiveRecord::Schema.define(version: 20161011213835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "services", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "type"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string   "email",                      null: false
