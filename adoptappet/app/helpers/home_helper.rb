80c656294506cdb715badb6bd4dd1ba168a988a1
dede999 <yankees666suck>
2016-11-19 13:33:37 -0200

    (views) creating the home view

--- adoptappet/app/helpers/home_helper.rb
+++ adoptappet/app/helpers/home_helper.rb
@@ -1,2 +1,14 @@
 module HomeHelper
+  def greeting time
+    case time
+      when 0..5, 19..23
+        return "Boa Noite"
+      when 6..12
+        return "Bom Dia"
+      when 13..18
+        return "Boa Tarde"
+      else
+        return "André, você esqueceu algo"
+    end
+  end
 end
